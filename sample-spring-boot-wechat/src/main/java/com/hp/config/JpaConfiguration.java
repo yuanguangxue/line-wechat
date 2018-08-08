4738648cdef29c9b5a09702c27f283b1452f3d51
c45_100006197808_qwe1372020 <c45_100006197808_qwe1372020@git.cloud.tencent.com>
2018-08-08 17:25:56 +0800

    'change'

--- sample-spring-boot-wechat/src/main/java/com/hp/config/JpaConfiguration.java
+++ sample-spring-boot-wechat/src/main/java/com/hp/config/JpaConfiguration.java
@@ -1,3 +1,4 @@
+/*
 package com.hp.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -57,3 +58,4 @@ public class JpaConfiguration {
         return hibernateJpaVendorAdapter;
     }
 }
+*/
